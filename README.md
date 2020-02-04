a4646abd9a7a11c316054be8242fffcae67546f7
Guangchuang Yu <guangchuangyu@gmail.com>
2020-02-04 19:04:41 +0800

    update docs

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-## 获取武汉肺炎疫情统计数据
+## Query information of Wuhan coronavirus infected stats
 
-具体使用方法，请参考<https://mp.weixin.qq.com/s/_0D8ENb-4lGm4UV16Ok28A>.
++ `get_nCov2019()` to query online latest information
++ `load_nCov2019()` to get historical data (updated at 2020/02/02)
+
+see the following documents for more details:
+
++ <https://mp.weixin.qq.com/s/_0D8ENb-4lGm4UV16Ok28A>
++ <https://mp.weixin.qq.com/s/lrQWGKj-mReWrxfi_4Sw9A>
 
--- inst/prepareHistoricalData.R
+++ inst/prepareHistoricalData.R
@@ -4,9 +4,10 @@ colnames(x)[1:6] <- c("province", "city", "time",
                       "cum_confirm", "cum_heal", "cum_dead")
 colnames(x)[9:11] <- c("confirm", "heal", "dead")
 
+x$time = as.Date(sub("(\\d+)\\D+(\\d+)\\D+", "\\1-\\2", 
+                   x$time), "%m-%d")
 
 object <- structure(list(data = x,
-                         time = as.Date(sub("(\\d+)\\D+(\\d+)\\D+", "\\1-\\2", 
-                                            x[1,3]), "%m-%d")
+                         time = x$time[1]
                          ), class = "nCov2019History")
 saveRDS(object, file="nCov2019History.rds")
