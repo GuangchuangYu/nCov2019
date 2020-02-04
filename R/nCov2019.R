cc02aa47aeb072562261b5e146c3ee2dc5c7ffd8
Patrick Tung <tung_patrick@yahoo.com>
2020-02-04 01:31:27 -0800

    test bug

--- R/nCov2019.R
+++ R/nCov2019.R
@@ -86,13 +86,13 @@ extract_province <- function(object, i, by) {
   x <- sub("\\)$", "", x)
   y <- jsonlite::fromJSON(x)
   
-  countriesurl <- jsonlite::fromJSON('https://gist.githubusercontent.com/jacobbubu/060d84c2bdf005d412db/raw/845c78f55e49fee89814bdc599355069f07b7ee6/countries.json')
-  countries <- countriesurl[, c("English", "China")]
-  countries$China <-gsub("；.*","",countries$China)
-  
-  y$data$areaTree$name <- plyr::mapvalues(y$data$areaTree$name,
-                                          from=countries$China,
-                                          to=countries$English)
+  # countriesurl <- jsonlite::fromJSON('https://gist.githubusercontent.com/jacobbubu/060d84c2bdf005d412db/raw/845c78f55e49fee89814bdc599355069f07b7ee6/countries.json')
+  # countries <- countriesurl[, c("English", "China")]
+  # countries$China <-gsub("；.*","",countries$China)
+  # 
+  # y$data$areaTree$name <- plyr::mapvalues(y$data$areaTree$name,
+  #                                         from=countries$China,
+  #                                         to=countries$English)
   return(y$data)  
 }
 
