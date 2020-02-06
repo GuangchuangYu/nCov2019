f02a61cb22e5881678aacd0189720c9c594900b9
Guangchuang Yu <guangchuangyu@gmail.com>
2020-02-06 14:02:26 +0800

    plot

--- R/utilities.R
+++ R/utilities.R
@@ -1,3 +1,28 @@
+get_city_data <- function(x, region, date) {
+    if (is(x, "nCov2019")) {
+        stats <- x[region, ]
+    } else {
+        stats <- extract_history(x, region, date)
+    }
+    names(stats)[1] <- 'NAME'
+    return(stats)
+}
+
+
+
+extract_history <- function(x, province, date) {
+  if (missing(province)) {
+    df <- summary(x)[, 1:3]
+  } else {
+    df <- x[province, 2:4]
+  }  
+  
+  df <- df[df$time == as.Date(date, "%Y-%m-%d"), c(1,3)]
+  names(df) <- c("name", "confirm")
+  return(df)
+}  
+
+
 extract_province <- function(object, i, by) {
   if (i == 'global') {
     res <- cbind(name = object$areaTree[[1]], object$areaTree[[by]])
@@ -20,5 +45,26 @@ extract_province <- function(object, i, by) {
   x <- sub("\\)$", "", x)
   y <- jsonlite::fromJSON(x)
   return(y$data)  
+}
+
+##' @importFrom ggplot2 scale_fill_gradient
+fill_scale_continuous <- function(palette = "Reds") {
+    cols = RColorBrewer::brewer.pal(6, palette)
+    breaks = c(1, 10, 100, 1000, 10000)
+    scale_fill_gradient(low=cols[1], high=cols[6],
+                na.value='white', trans='log',
+                breaks=breaks, labels=breaks)
+}
+
+discrete_breaks <- c(1,10,100,500,10^3,10^4, 10^5)
+
+##' @importFrom ggplot2 scale_fill_brewer
+fill_scale_discrete <- function(palette = "Reds") {
+    scale_fill_brewer(palette=palette, name='confirm',
+            na.translate = FALSE,
+            breaks = c('[1,10)', '[10,100)', '[100,500)', 
+                      '[500,1e+03)', '[1e+03,1e+04)', '[1e+04,1e+05]'),
+            labels = c("<10", "10-100", "100-500", "500-1000", 
+                        "1000-10000", ">10000"))
 }
 
---
+++ inst/setup.R
@@ -0,0 +1,35 @@
+setup_chinamap <- function(cn) {
+    cn$province = sub("省", "", cn$province)
+    cn$province = sub("自治区", "", cn$province)
+    cn$province = sub("市", "", cn$province)
+    cn$province = sub("特别行政区", "", cn$province)
+    cn$province = sub("维吾尔", "", cn$province)
+    cn$province = sub("壮族", "", cn$province)
+    cn$province = sub("回族", "", cn$province)
+    return(cn)
+}
+
+setup_city <- function(map) {
+    map <- tibble::as_tibble(m)
+    map$NAME <- as.character(map$NAME)
+    map$NAME <- sub("市$", "", map$NAME)
+    map$NAME <- sub("地区$","", map$NAME)
+    map$NAME <- sub("省$","", map$NAME)
+    map$NAME <- sub("特别行政区$","", map$NAME)
+    map$NAME <- sub("土家族","", map$NAME)
+    map$NAME <- sub("蒙古族","", map$NAME)
+    map$NAME <- sub("哈尼族","", map$NAME)
+    map$NAME <- sub("布依族","", map$NAME)
+    map$NAME <- gsub(".族","", map$NAME)
+    map$NAME <- sub("自治","", map$NAME)
+    map$NAME[map$NAME == '神农架林区'] = '神农架'
+    map$NAME[map$NAME == '甘孜州'] = '甘孜'
+    map$NAME[map$NAME == '凉山州'] = '凉山'
+    return(map)
+}
+
+ncovEnv = new.env()
+assign("setup_chinamap", setup_chinamap, envir = ncovEnv)
+assign("setup_city", setup_city, envir = ncovEnv)
+
+save(ncovEnv, file="ncovEnv.rda")
