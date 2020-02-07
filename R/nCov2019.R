8a88c92ebd49d7dff8577abd4320c781c598d8a0
Patrick Tung <tung_patrick@yahoo.com>
2020-02-07 13:09:38 -0800

    add english provinces in load_ncov (fixing bug) revert

--- R/nCov2019.R
+++ R/nCov2019.R
@@ -43,8 +43,8 @@ load_nCov2019 <- function(lang = 'zh') {
   
   #prov_cities <- jsonlite::fromJSON(system.file('provinces_and_cities.json', package="nCov2019"))
   
-  if (lang == 'en') {
+  #if (lang == 'en') {
     # change provinces to English
-    data$data = transform(data$data, province = prov_cities$province_name_en[match(province, prov_cities$province_name_zh)])
-  }
+  #  data$data = transform(data$data, province = prov_cities$province_name_en[match(province, prov_cities$province_name_zh)])
+  #}
 }
