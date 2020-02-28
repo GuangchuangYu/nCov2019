2b2c108a3d5eb825b9988be738d1fd24f42a483b
Guangchuang Yu <guangchuangyu@gmail.com>
2020-02-29 00:34:13 +0800

    typo

--- R/dashboard.R
+++ R/dashboard.R
@@ -1,7 +1,7 @@
 open_dashboard <- function(lang="auto", remote=FALSE) {
     lang <- which_lang(lang) #zh or en
     if (remote) {
-        if (lang = 'zh') {
+        if (lang == 'zh') {
             utils::browseURL('http://www.bcloud.org/v/')
         } else {
             utils::browseURL('http://www.bcloud.org/e/')
